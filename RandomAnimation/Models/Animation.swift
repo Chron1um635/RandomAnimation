937bf9c3cd53407722634ad0ec401df0dd410bde
Chron1um635 <chron1um522@gmail.com>
2024-09-19 19:59:08 +0300

    setup UI

---
+++ RandomAnimation/Models/Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  RandomAnimation
+//
+//  Created by Максим Назаров on 19.09.2024.
+//
+
+import Foundation
---
+++ RandomAnimation/Services/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  RandomAnimation
+//
+//  Created by Максим Назаров on 19.09.2024.
+//
+
+import Foundation
