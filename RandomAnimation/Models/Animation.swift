f54c3d5f9727fec97736798a5801d9fece43a98e
Chron1um635 <chron1um522@gmail.com>
2024-09-19 19:59:46 +0300

    setup model

--- RandomAnimation/Models/Animation.swift
+++ RandomAnimation/Models/Animation.swift
@@ -5,4 +5,29 @@
 //  Created by Максим Назаров on 19.09.2024.
 //
 
-import Foundation
+struct Animation {
+    let preset: String
+    let curve: String
+    let force: Double
+    let duration: Double
+    let delay: Double
+    
+    static func getAnimations() -> [Animation] {
+        let data = DataStore.shared
+        var animations: [Animation] = []
+        
+        for _ in 0...100 {
+            animations.append(
+                Animation(
+                    preset: data.animations[Int.random(in: 0..<data.animations.count)],
+                    curve: data.curves[Int.random(in: 0..<data.curves.count)],
+                    force: Double.random(in: 1...1.5),
+                    duration: Double.random(in: 0.5...5),
+                    delay: Double.random(in: 0...1)
+                )
+            )
+        }
+        
+        return animations
+    }
+}
