3da46b471bf909da43f2c272d7c60224e492c713
Chron1um635 <chron1um522@gmail.com>
2024-09-20 18:52:03 +0300

    update logic

--- RandomAnimation/ViewControllers/ViewController.swift
+++ RandomAnimation/ViewControllers/ViewController.swift
@@ -12,39 +12,32 @@ final class ViewController: UIViewController {
 
     @IBOutlet var animatedView: SpringView!
     
-    @IBOutlet var presetLabel: UILabel!
-    @IBOutlet var curveLabel: UILabel!
-    @IBOutlet var durationLabel: UILabel!
-    @IBOutlet var delayLabel: UILabel!
-    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
     
     @IBOutlet var runAnimationButton: UIButton!
     
-    private let animations = Animation.getAnimations()
-    private var animation: Animation!
+    private var animation = Animation.getAnimation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         animatedView.layer.cornerRadius = 10
         runAnimationButton.layer.cornerRadius = 10
         
-        animation = animations[Int.random(in: 0..<animations.count)]
-        
-        setAnimationValuesToLabels(animation)
+        descriptionLabel.text = animation.description
     }
 
     @IBAction func runAnimationButtonTapped() {
         if runAnimationButton.titleLabel?.text == "Run" {
             animateView(animatedView, with: animation)
             
-            animation = animations[Int.random(in: 0..<animations.count)]
+            animation = Animation.getAnimation()
             runAnimationButton.setTitle("Run \(animation.preset)", for: .normal)
         } else {
             animateView(animatedView, with: animation)
             
-            setAnimationValuesToLabels(animation)
+            descriptionLabel.text = animation.description
             
-            animation = animations[Int.random(in: 0..<animations.count)]
+            animation = Animation.getAnimation()
             runAnimationButton.setTitle("Run \(animation.preset)", for: .normal)
         }
     }
@@ -58,18 +51,6 @@ private extension ViewController {
         view.delay = animation.delay
         view.force = animation.force
         view.animate()
-    }
-    
-    func setAnimationValuesToLabels(_ animation: Animation) {
-        presetLabel.text = "Preset: \(animation.preset)"
-        curveLabel.text = "Curve: \(animation.curve)"
-        durationLabel.text = "Duration: \(string(from: animation.duration))"
-        delayLabel.text = "Delay: \(string(from: animation.delay))"
-        forceLabel.text = "Force: \(string(from: animation.force))"
-    }
-    
-    func string(from value: Double) -> String {
-        String(format: "%.2f", value)
     }
 }
 
